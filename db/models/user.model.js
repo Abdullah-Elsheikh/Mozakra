3a5927bc70f2feea4d0bdd3ed4cbbfb05629a2ed
Abdullah42021231 <bda163587@gmail.com>
2025-12-08 12:53:50 +0200

    add phone in user schema

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -13,6 +13,10 @@ const userSchema = new Schema({
         type : String,
         trim : true,
     },
+    phone :{
+        type : String,
+        trim : true,
+    },
     password :{
         type : String,
         required: true,
