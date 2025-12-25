aeb830be406d0186ee85d44896d89cd1687d68cb
Abdullah42021231 <bda163587@gmail.com>
2025-12-25 14:27:11 +0200

    fix save in databas

--- db/models/list.model.js
+++ db/models/list.model.js
@@ -3,7 +3,9 @@ import { model, Schema } from "mongoose";
 const listSchema = new Schema({
     name:{
     type:String,
-    }
-},{timestamps: true});
+    },
+     pdfUrl: { type: String, default: null },
+  pdfPublicId: { type: String, default: null }
+},{timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true }});
 
 export const List = model("List",listSchema);
