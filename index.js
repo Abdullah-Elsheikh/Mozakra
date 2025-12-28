09f2d50b5d4444b1540f5a04563679571ba3193d
Abdullah42021231 <bda163587@gmail.com>
2025-12-28 16:21:28 +0200

    add cors in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
 import dotenv from 'dotenv';
+import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from "url";
 import { connectDB } from './db/connection.js';
@@ -20,6 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
